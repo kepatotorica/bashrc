a37ced1565b8f242e3161b1ad2068b5ca4b75865
kepa totorica <34106186+kepatotorica@users.noreply.github.com>
2021-03-10 13:56:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 echo "Moving bashrc"
-cp .bashrc ~/.bashrc
+scp .bashrc ~/.bashrc
 echo "Moving settings.json for windows terminal"
-cp settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+scp settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
 source ~/.bashrc
 echo "installing bash theme"
 mkdir ~/.bash/themes/PL/
-cp theme.bash ~/.bash/themes/PL/theme.bash
+scp theme.bash ~/.bash/themes/PL/theme.bash
 echo "installing Powerline Fonts"
 installPowerlineFonts
 source ~/.bashrc
