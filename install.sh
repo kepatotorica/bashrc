217805e341c8908a442c1483f2094961b3670533
kepa totorica <34106186+kepatotorica@users.noreply.github.com>
2021-03-11 09:55:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ scp .bashrc ~/.bashrc
 echo "Moving settings.json for windows terminal"
 scp settings.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
 source ~/.bashrc
+echo "moving inputrc to remove visible blink"
+scp inputrc C:/Program\ Files/Git/etc/inputrc
 echo "installing bash theme"
 mkdir ~/.bash/themes/PL/
 scp theme.bash ~/.bash/themes/PL/theme.bash
